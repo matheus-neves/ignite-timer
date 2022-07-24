3f861bcdf51d2051861a7ac3c6e5f32530a92921
Matheus Neves <matheusneves1989@gmail.com>
2022-07-24 18:05:06 -0300

    feat(Route): create routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { ThemeProvider } from 'styled-components'
+import { Router } from './routes/Router'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <Router />
       <GlobalStyle />
-      <h1>Timer</h1>
     </ThemeProvider>
   )
 }
