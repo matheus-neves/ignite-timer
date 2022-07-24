f0afe3af2bb26c3f57ea0698d92f5bcf32c01e54
Matheus Neves <matneves@SAO-C02G620KML7H.local>
2022-07-24 17:51:32 -0300

    styles: format all files

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -10,12 +10,12 @@ export const GlobalStyle = createGlobalStyle`
 
   :focus {
     outline: 0;
-    box-shadow: 0 0 0 2px ${({theme}) => theme['green-500']};;
+    box-shadow: 0 0 0 2px ${({ theme }) => theme['green-500']};;
   }
 
   body {
-    background-color: ${({theme}) => theme['gray-900']};
-    color: ${({theme}) => theme['gray-300']};
+    background-color: ${({ theme }) => theme['gray-900']};
+    color: ${({ theme }) => theme['gray-300']};
   }
 
   body, input, textarea, button {
