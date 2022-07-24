c4e5ca32d2e519c5d65931c063520d46f352e035
Matheus Neves <matheusneves1989@gmail.com>
2022-07-24 18:11:35 -0300

    feat(Route): add default layout

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,13 +1,16 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import { Home } from '../pages/Home'
 import { History } from '../pages/History'
+import { DefaultLayout } from '../layouts/DefaultLayout'
 
 export function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/history" element={<History />} />
+        <Route path="/" element={<DefaultLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/history" element={<History />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
