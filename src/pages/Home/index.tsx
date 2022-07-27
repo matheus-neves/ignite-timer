17ff54727540ada519cbf91d81fe6e4c5f82e62e
Matheus Neves <matheusneves1989@gmail.com>
2022-07-26 22:16:26 -0300

    feat(Home): structure and styles

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,45 @@
+import { Play } from 'phosphor-react'
+import {
+  CountDownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  Separator,
+  StartCountDownButton,
+  TaskInput,
+} from './styles'
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form>
+        <FormContainer>
+          <label htmlFor="task">I will work on</label>
+          <TaskInput id="task" placeholder="give your project a name" />
+
+          <label htmlFor="minutesAmount">for</label>
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+          />
+
+          <span>minutes.</span>
+        </FormContainer>
+
+        <CountDownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountDownContainer>
+
+        <StartCountDownButton type="submit">
+          <Play size={24} />
+          Start
+        </StartCountDownButton>
+      </form>
+    </HomeContainer>
+  )
+}
